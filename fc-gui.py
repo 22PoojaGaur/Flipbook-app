e8fff64a684cc8191c8e0d41683d84e215a7c17b
22PoojaGaur <poojagaur2293@gmail.com>
2020-12-06 16:40:17 +0530

    updated basic gui

--- fc-gui.py
+++ fc-gui.py
@@ -8,20 +8,23 @@ def set_program_name(event):
     print(program_name.get())
     print(output_name.get())
 
+    Alert.set("Compiling you program...")
     fc.start(program_name.get(), output_name.get())
+    Alert.set("check generated output file. Thanks!")
 
 
 if __name__ == '__main__':
-    window = tk.Tk()
+    window = tk.Tk(className="FlipBook Generator")
+    window.geometry("400x400")
 
-    label_enter_program_fname = tk.Label(text="Enter name of .flip program file")
-    program_name = tk.Entry()
+    label_enter_program_fname = tk.Label(text="Enter relative path to .flip program file")
+    program_name = tk.Entry(width=100)
 
     label_enter_program_fname.pack()
     program_name.pack()
 
     label_enter_output_fname = tk.Label(text="Enter name of output file")
-    output_name = tk.Entry()
+    output_name = tk.Entry(width=100)
 
     label_enter_output_fname.pack()
     output_name.pack()
@@ -36,5 +39,9 @@ if __name__ == '__main__':
 
     button.bind("<Button-1>", set_program_name)
     button.pack()
+
+    Alert = tk.StringVar()
+    alert_label = tk.Label(textvariable=Alert)
+    alert_label.pack()
 
     window.mainloop()
